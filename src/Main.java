0bda867e87a76a9eb2ec595acde8e1dcb21b8faf
Elvira <adilovaelvira123@gmail.com>
2023-04-06 03:33:13 +0600

    calculating the factorial of the number by using recursive method

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,22 @@
 import java.util.Scanner; //importing scanner from package
 public class Main{
-    public static boolean func(int a) {
-        return Prime(a,2); //calling function Prime and giving the arguments
-    }
-
     /**
-     * Assesing if a given number is prime or not
-     * @param a The value to check whether it is prime or not
-     * @param b The divisor to check if value a is divisible by value b
-     * @return True if value a is prime, false otherwise
+     * Calculating the factorial of a number by recursive method
+     * @param a the value to calculate the factorial
+     * @return the factorial of the value
      */
-    private static boolean Prime(int a, int b) //recursive function
-    {
-        if (a <= 1 || a % b == 0) {  // checking if the number is less than or equal to 1 or if it is divisible by b
-            return false;
+        public static int func(int a) {
+            if (a == 0) { // base case when number is equal to zero it returns 1
+                return 1;
+            }
+            else {
+                return a * func(a-1); // calling func recursively to find factorial
+            }
         }
-        if (b * b > a) { //checking if the square of b is greater than a
-            return true;
-        }
-        return Prime(a,b + 1); // if both conditions aren't satisfied, call the function recursively
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); //creating an object of the Scanner class
-        int n = sc.nextInt(); //taking inout value from user
-        boolean Prime = func(n); //assessing if the number is prime
-
-        if (Prime) {
-            System.out.println("Prime"); //if true, then prime
-        } else {
-            System.out.println("Composite"); //if false, then composite
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in); //creating an object of the Scanner class
+            int n = sc.nextInt(); //taking user's input and storing it in variable n
+            int factorial = func(n); //calculating the factorial
+            System.out.println(factorial); //printing the factorial
         }
     }
-}
