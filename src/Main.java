da89aeb707fb6d1cc729ccb0db4671a89cef6175
Elvira <adilovaelvira123@gmail.com>
2023-04-09 16:28:22 +0600

    Finding n-th element of Fibonacci sequence by using recursion

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,9 @@ public class Main {
             case 4:
                 Task4.task4_main();
                 break;
+            case 5:
+                Task5.task5_main();
+                break;
             default:
                 System.out.println("please enter number from 1 to 10"); //if the number doesn't match print message
                 break;
@@ -155,6 +158,34 @@ class Task4 {
             return 1; // base case when number is equal to zero it returns 1
         } else {
             return a * isFactorial(a - 1); // calling func recursively to find factorial
+        }
+    }
+}
+
+class Task5 {
+    public static void task5_main(){
+        Scanner sc = new Scanner(System.in);  //creating a Scanner instance
+        System.out.print("Enter a number: "); //getting a value from user
+        int n = sc.nextInt(); //storing the input in variable n
+
+        int fbnc = findFibonacci(n); //calling the findFibonacci to calculate the value of n-th element
+        System.out.println(fbnc); //printing the n-th element of the sequence
+    }
+
+    /**
+     * Finding n-th element of Fibonacci sequence by using recursion
+     * @param n index of the element in the sequence
+     * @return the value of element with index n
+     */
+    public static int findFibonacci(int n) {
+        if (n == 0) {
+            return 0; //base case when n is zero, result is 0
+        }
+        else if (n == 1) {
+            return 1; //base case when n is one, result is one
+        }
+        else {
+            return findFibonacci(n-1) + findFibonacci(n-2); //recursively calculating using the formula
         }
     }
 }
