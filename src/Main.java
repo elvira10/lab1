0f18e052573bc5d038ffc1cab225ff958a5ead38
Elvira <adilovaelvira123@gmail.com>
2023-04-09 15:08:27 +0600

    Finding the minimum number in an int array using recursion

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,48 @@
 import java.util.Scanner; //importing scanner from package
 public class Main {
-    /**
-     * Finding n-th element of Fibonacci sequence by using recursion
-     * @param n index of the element in the sequence
-     * @return the value of element with index n
-     */
-    public static int func(int n) {
-        if (n == 0) { //base case when n is zero, result is 0
-            return 0;
-        }
-        else if (n == 1) { //base case when n is one, result is one
-            return 1;
-        }
-        else {
-            return func(n-1) + func(n-2); //recursively calculating using the formula
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        switch (a){
+            case 1:
+                Task1.task1_main();
+                break;
+            default:
+                System.out.println("please enter number from 1 to 10");
+                break;
         }
     }
+}
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); //creating an object of the Scanner class
-        int n = sc.nextInt(); //taking user's input and storing it in variable n
-        int fbnc = func(n); //calling the function to calculate the value of n-th element
-        System.out.println(fbnc); //printing the n-th element of the sequence
+class Task1 {
+    public static void task1_main(){
+        Scanner scanner = new Scanner(System.in); //creating a Scanner instance
+        System.out.println("Enter the length of an array");  //getting the length of an array from user
+        int length = scanner.nextInt(); //storing the input in length variable
+        int[] arr = new int[length]; //filling the array with the values stored
+
+        System.out.println("Enter the values of an array"); //getting the values of an array from user
+        for (int i=0; i<length; i++) //iterate the array staring with index "0" to "length-1"
+        {
+            arr[i] = scanner.nextInt(); //reading the values by Scanner
+        }
+        int minimum = findMin(arr, length); //calling the findMin function to calculate the minimum value
+        System.out.println(minimum); //printing the result
+    }
+
+    /**
+     * Finding the minimum number in an int array using recursion
+     * @param arr the integer array
+     * @param n the length of the array
+     * @return the minimum number in the array
+     */
+
+    public static int findMin(int[] arr, int n){
+    if (n == 1)
+        return arr[0]; //if array has only one value, return it as an answer
+        else {
+        int min = findMin(arr, n - 1); //calling the function recursively
+        return Math.min(min, arr[n-1]); //assessing the smallest value identified until now with the last element in array
+        }
     }
 }
